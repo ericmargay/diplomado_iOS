74c4ca09ed6dd75eac407ef47f4f228f71240077
ericmargay <ericmargay@gmail.com>
2023-08-18 17:03:33 -0600

    Init

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,14 @@
+import UIKit
+
+var greeting = ""
+// Palabra var reservada para variables
+let constante = "Helplayground"
+// Palabra let reservada para constantes
+
+greeting = "hgoldsf"
+
+
+        
+var miEnteroUno: Int
+// best practice de como declarar la variable
+
