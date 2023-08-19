41c1689b99f1aed12dc1b307bf51db5ac929a997
ericmargay <ericmargay@gmail.com>
2023-08-19 14:10:58 -0600

    Uploading Grecia Class with tasks

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -13,3 +13,35 @@ var miEnteroUno: Int
 // best practice de como declarar la variable
 
 var github: String
+
+if #available(iOS 16.6, *) {
+    // Only for this
+}
+else {
+    
+}
+var number: Int
+
+//number == 2 ? print(" is 2") : print("Other number")
+
+//var value = number == 2 ? number : 4
+
+
+
+// Crear una variable llamada weather que puede tener 2 posibles valores lluvia y nieve. De igual modo 😀, crear una variable, "time", con la hora del dia (0-23).
+
+// Si es nieve imprimir en pantalla "nubecopos", si es lluvia imprimir "nubegotas".
+//De 7-18 imprimir "Usa bloqueador" y de 19-6 imprimir "No necesitas bloqueador"
+
+// Para casos no contemplados no hacer algo.
+var weather = "lluvia"
+var time = 12
+
+var dark = Array(0...6) + Array(19...23)
+var light = 7...18
+
+
+weather == "lluvia" ? print("❄️") : print("🌨️")
+
+light ~= time ? print("Usa bloqueador") : print("No necesitas bloqueador")
+
